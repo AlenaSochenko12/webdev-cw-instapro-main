34ee9aac0325ef01a51bc4530c6c4b22e12fae70
AlenaSochenko12 <108708009+AlenaSochenko12@users.noreply.github.com>
2025-02-26 14:16:01 +1000

    New changes

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ export const goToPage = (newPage, data) => {
           page = POSTS_PAGE;
           posts = newPosts;
           renderApp();
+          //
         })
         .catch((error) => {
           console.error(error);
